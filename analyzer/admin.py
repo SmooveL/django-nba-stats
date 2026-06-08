59d2c52c66e6f97693c2ec7b424b580cb8e68283
SmooveL <artemkurusin23@gmail.com>
2026-06-09 02:13:46 +0500

    add models to django admin

--- analyzer/admin.py
+++ analyzer/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Team, Player, SeasonStat
 
-admin.site.register(Team)
-admin.site.register(Player)
-admin.site.register(SeasonStat)
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'abbreviation', 'conference')
+    search_fields = ('name', 'city', 'abbreviation')
+    list_filter = ('conference',)
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'team', 'position')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('team', 'position')
+
+@admin.register(SeasonStat)
+class SeasonStatAdmin(admin.ModelAdmin):
+    list_display = ('player', 'season_year', 'games_played', 'per')
+    search_fields = ('player__first_name', 'player__last_name')
